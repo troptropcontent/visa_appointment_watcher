5a7d1933f4901dddbc50db6d9279c5b1f4326812
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-10 09:49:33 +0000

    setup config.ROOT_DIR

--- internal/config/initializer.go
+++ internal/config/initializer.go
@@ -1,11 +1,15 @@
 package config
 
 import (
+	"path/filepath"
+	"runtime"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+var ROOT_DIR string
 
 type Config struct {
 	gorm.Model
@@ -13,9 +17,15 @@ type Config struct {
 	Value string
 }
 
+func findRootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	return filepath.Join(filepath.Dir(b), "../..")
+}
+
 // Init initializes the config database, it returns an error if it fails
 func Init() error {
-	db, err := gorm.Open(sqlite.Open("./storage/config.db"), &gorm.Config{})
+	ROOT_DIR = findRootDir()
+	db, err := gorm.Open(sqlite.Open(ROOT_DIR+"/storage/config.db"), &gorm.Config{})
 	if err != nil {
 		return err
 	}
