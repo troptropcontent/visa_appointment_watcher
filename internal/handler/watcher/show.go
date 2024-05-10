efcb3f557c35fd0e347ecea3ef33c64786ba370f
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-10 15:46:43 +0000

    update show

--- internal/handler/watcher/show.go
+++ internal/handler/watcher/show.go
@@ -34,12 +34,14 @@ func loadLogs() []string {
 // Show returns the status of the watcher and the recent logs
 func Show(c echo.Context) error {
 	is_activated, _ := config.Get("watcher_running")
+	last_appointment_date_found, _ := config.Get("last_appointment_date_found")
+	last_appointment_date_found_at, _ := config.Get("last_appointment_date_found_at")
 	data := &watcher_show_template.Template{
 		Title:                      "Watcher",
 		Logs:                       parseLogs(),
 		IsActivated:                is_activated == "true",
-		LastAppointmentDateFound:   config.MustGet("last_appointment_date_found"),
-		LastAppointmentDateFoundAt: config.MustGet("last_appointment_date_found_at"),
+		LastAppointmentDateFound:   last_appointment_date_found,
+		LastAppointmentDateFoundAt: last_appointment_date_found_at,
 	}
 	return c.Render(http.StatusOK, "watcher/show", data)
 }
