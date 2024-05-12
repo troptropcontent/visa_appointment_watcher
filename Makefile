af0478cc55290f743ccd4c2c55136944776983d7
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-12 19:23:19 +0000

    setup tailwind in development environment

---
+++ Makefile
@@ -0,0 +1,2 @@
+tailwind:
+	./bin/tailwindcss -i ./internal/assets/stylesheets/tailwind_input.css -o ./public/css/tailwind_output.css --watch
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./internal/views/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
