5ed7851dca477548f11cd8e9b3b43c94903429a1
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-16 19:34:33 +0000

    wait for the scrapper to be finished

--- internal/lib/watcher/browser.go
+++ internal/lib/watcher/browser.go
@@ -1,15 +1,10 @@
 package watcher
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
-	"github.com/troptropcontent/visa_appointment_watcher/internal/config"
 )
 
 const HEADLESS_BROWSER_URL_DEVELOPMENT = "http://headlessBrowser:7317"
@@ -44,30 +39,6 @@ func NewBrowser(scrapper *Scrapper) Browser {
 	client := launcher.MustClient()
 
 	browser := rod.New().Client(client).MustConnect()
-
-	router := browser.HijackRequests()
-
-	router.MustAdd("*/en-fr/niv/schedule/*/appointment/days/*.json*", func(ctx *rod.Hijack) {
-		if err := ctx.LoadResponse(http.DefaultClient, true); err != nil {
-			return
-		}
-		responseBody := ctx.Response.Body()
-		var parsedResponse AppointmentDateResponse
-		json.Unmarshal([]byte(responseBody), &parsedResponse)
-		rawDate := parsedResponse[0].Date
-		parsedDate, err := time.Parse("2006-01-02", rawDate)
-		if err != nil {
-			return
-		}
-
-		scrapper.Logger.Info().Msg(fmt.Sprintf("Next appointment date found: %s", parsedDate))
-
-		config.MustSet("last_appointment_date_found", parsedDate.Format("2006-01-02"))
-		config.MustSet("last_appointment_date_found_at", time.Now().Format(time.RFC3339))
-		scrapper.NextDate = parsedDate
-	})
-
-	go router.Run()
 
 	return Browser{browser: browser, Scrapper: scrapper}
 }
